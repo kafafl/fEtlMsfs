8a8c2a13ce3c97d5e20aac3312afa430cb40c474
kafafl <Lee.Kafafian@allosteryinvest.com>
2023-12-11 14:25:41 -0500

    Initial check-in

---
+++ function_app.py
@@ -0,0 +1,19 @@
+import logging
+import azure.functions as func
+import datetime
+
+app = func.FunctionApp()
+
+@app.schedule(schedule="*/60 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+
+def fxTest(myTimer: func.TimerRequest) -> None:
+    
+    dtGetDate = datetime.datetime.now()
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
+    
+    print(dtGetDate.strftime(r'%m/%d/%y %H:%M:%S') + ":  Confirmed debug cycle in python...")
+
+
